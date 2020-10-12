7271b1ce4d9d2d4df0fb7f5fad864bfb7122af03
Molinari <matteomolinari93@gmail.com>
2020-10-12 10:52:57 +0200

    Degenerate Objects

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -9,12 +9,11 @@ public class MoneyTest {
 	@Test
 	void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
 		
-		five = new Dollar(5);
-		five.times(5);
-		assertEquals(25, five.amount);
+		product = five.times(5);
+		assertEquals(25, product.amount);
 	}
 
 }
